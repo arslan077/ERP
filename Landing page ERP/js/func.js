bfc15bfdae51ad1fa96ed01a8c8d57b226acb56f
arslan077 <arslan9971@gmail.com>
2023-03-01 22:03:21 +0500

    theme

---
+++ Landing page ERP/js/func.js
@@ -0,0 +1,9 @@
+function moveleft(){
+    
+    var container =document.getElementById("container");
+    container.classList.add("right-panel-active");
+}
+function moveright(){
+    var container=document.getElementById("container")
+    container.classList.remove("right-panel-active");
+} 
